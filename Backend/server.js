9ca33ce03d2c70435b3209ae487425416a242de4
adit1611 <aditya16112002@gmail.com>
2024-12-24 10:48:31 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Configure CORS
 const corsOptions = {
-    origin: 'https://sign-login-pract-vt.vercel.app', // Specify the frontend origin
+    origin: 'https://sign-login-pract-vt.vercel.app/', // Specify the frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     };
 
